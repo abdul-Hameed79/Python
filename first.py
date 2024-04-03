27a00429e032b7fa0db191d8ca98fb00c8a7d801
Hameed <sd.hameed79@gmail.com>
2024-04-03 23:32:11 +0530

    Successfully Modified

--- first.py
+++ first.py
@@ -1,1 +1,5 @@
 print("Hello World")
+
+# In other programming languages we should write so many lines of code to print a simple "Hello world" program.
+# but in python there is no need to write so many of code to for a simple program.
+# Python is famuos ecause of it's simple structure and readiability.
