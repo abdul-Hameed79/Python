10516708fc598353233cd59ab25308fa25f4f334
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-13 14:09:15 +0530

    Strings topic added

---
+++ Strings/Concatenation.py
@@ -0,0 +1,9 @@
+
+firstName = "Prabhas"
+lastName = "is a PAN India star"
+
+fullName = firstName + " " + lastName
+
+print(fullName)         #concatenate
+print(len(fullName))    #length of string
+print(fullName[6])      #index of string
---
+++ Strings/Slicing.py
@@ -0,0 +1,9 @@
+name = "Virat Kohli"
+print(name[5:len(name)])
+
+#output: kohli
+
+Name = "Apple"
+print(Name[-3:-1])
+
+#output: pl
