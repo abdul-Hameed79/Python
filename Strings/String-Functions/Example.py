5b43aa5363376a12c6c627581b4ec238d124b949
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-13 14:32:57 +0530

    string functions added

---
+++ Strings/String-Functions/Example.py
@@ -0,0 +1,12 @@
+str = "i have started my Python journey"
+
+print(str.capitalize())     #captializes first character
+
+print(str.endswith("ey"))   #return True if the sentence ends with ey
+
+print(str.replace("my", "our"))    #replaces the old with new
+
+print(str.find("j"))    #gives the index of word
+
+print(str.count("a"))   #counts the occurence of substring
+
