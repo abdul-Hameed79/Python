3632d62fb47621dfc38415acd1f3b6e5295db04b
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-18 23:59:12 +0530

    Update age example problem

--- Condition_Statements/Age_Example.py
+++ Condition_Statements/Age_Example.py
@@ -1,5 +1,12 @@
+# age = 20
 
-age = 20
+# if(age >= 18): print("Eligible to vote")
+# else: print("Not eligible")
 
-if(age >= 18): print("Eligible to vote")
-else: print("Not eligible")
+def is_adult(age):
+    return True if age > 18 else False
+print(is_adult(34))
+
+# def is_eligible(Age):
+#      return ("Eligible") if Age > 18 else ("Not Eligible)"
+#print(is_eligible(23))
