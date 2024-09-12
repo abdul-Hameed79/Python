a8edb9542a9bd58bec5351be83fc7b5d7389144d
Abdul Hameed <sd.hameed79@gmail.com>
2024-09-12 11:16:13 +0530

    using for loop with different statement examples

--- Loops/ForLoop.py
+++ Loops/ForLoop.py
@@ -1,3 +1,21 @@
 cars = ["BMW", "Mercedes", "TATA", "Tesla", "Mahindra"]
 for i in cars:
-    print(i)
+    print(i)    #Output: BMW Mercedes TATA Tesla Mahindra
+print("")
+
+
+# using break statement
+cars = ["BMW", "Mercedes", "TATA", "Tesla", "Mahindra"]
+for x in cars:
+    if x == "TATA":
+        break
+    print(x)    #Output: BMW Mercedes
+print("")
+
+
+# Using Continue statement
+cars = ["BMW", "Mercedes", "TATA", "Tesla", "Mahindra"]
+for y in cars:
+    if y == "Mercedes":
+        continue
+    print(y)
